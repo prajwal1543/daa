644ea672f6718ea05dd3a15584046fe0442045ed
prajwal1543 <ipcworshipmusic123@gmail.com>
2024-07-15 21:51:57 +0530

    Create maxpeople.py

---
+++ maxpeople.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumPeople' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. LONG_INTEGER_ARRAY p
+#  2. LONG_INTEGER_ARRAY x
+#  3. LONG_INTEGER_ARRAY y
+#  4. LONG_INTEGER_ARRAY r
+#
+
+def maximumPeople(p, x, y, r):
+    # Return the maximum number of people that will be in a sunny town after removing exactly one cloud.
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    x = list(map(int, input().rstrip().split()))
+
+    m = int(input().strip())
+
+    y = list(map(int, input().rstrip().split()))
+
+    r = list(map(int, input().rstrip().split()))
+
+    result = maximumPeople(p, x, y, r)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
